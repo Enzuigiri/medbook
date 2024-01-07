3899e065c8ef77d3f4561a863a0c44179f2bb1f8
Enzuigiri <zulfadliyunir.08@gmail.com>
2024-01-07 14:54:31 +0700

    feat: implement dependency injection in upload
    Add dependency injection for both storage and virus scanner in UploadRequest

--- hospital-backend/src/upload.ts
+++ hospital-backend/src/upload.ts
@@ -1,2 +1,32 @@
 import { Attachment, Storage } from "./storage";
 import { storage_config, storage_factory } from "./storage-factory";
+import {
+  ThreatProtecScanner,
+  SynergySecurityScanner,
+  Scanner,
+} from "./virus-scanner";
+
+const isProduction = process.env.NODE_ENV == "production";
+const virus_scanner = isProduction
+  ? new ThreatProtecScanner()
+  : new SynergySecurityScanner();
+
+class UploadRequest {
+  private readonly storage: Storage;
+  private readonly scanner: Scanner;
+
+  constructor(storage: Storage, scanner: Scanner) {
+    this.storage = storage;
+    this.scanner = scanner;
+  }
+
+  async upload(attachment: Attachment, mime_type: string) {
+    if (await this.scanner.scan(attachment)) {
+      throw Error("Virus scanner detected issue");
+    }
+    return await this.storage.upload(attachment);
+  }
+}
+
+const storage = storage_factory.create_factory("test");
+const request = new UploadRequest(storage, virus_scanner);
