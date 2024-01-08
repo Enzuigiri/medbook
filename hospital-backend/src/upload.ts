bf7a50d3d5cfe7e358a79876f9efa6ce4e5b6fb7
Enzuigiri <zulfadliyunir.08@gmail.com>
2024-01-08 11:14:45 +0700

    feat: add image scaler feature
    scale image file to 2500 width for every image that mime type supported

--- hospital-backend/src/upload.ts
+++ hospital-backend/src/upload.ts
@@ -1,3 +1,4 @@
+import { ImageScaler, SharpImageScaler } from "./image_scaler";
 import { Attachment, Storage } from "./storage";
 import { storage_config, storage_factory } from "./storage-factory";
 import {
@@ -10,23 +11,31 @@ const isProduction = process.env.NODE_ENV == "production";
 const virus_scanner = isProduction
   ? new ThreatProtecScanner()
   : new SynergySecurityScanner();
+const image_scaler = new SharpImageScaler()
 
 class UploadRequest {
   private readonly storage: Storage;
   private readonly scanner: Scanner;
+  private readonly scaller: ImageScaler;
 
-  constructor(storage: Storage, scanner: Scanner) {
+  constructor(storage: Storage, scanner: Scanner, scaller: ImageScaler) {
     this.storage = storage;
     this.scanner = scanner;
+    this.scaller = scaller
   }
 
   async upload(attachment: Attachment, mime_type: string) {
     if (await this.scanner.scan(attachment)) {
       throw Error("Virus scanner detected issue");
     }
+
+    if (this.scaller.supported(mime_type)) {
+
+    }
+    
     return await this.storage.upload(attachment);
   }
 }
 
 const storage = storage_factory.create_factory("test");
-const request = new UploadRequest(storage, virus_scanner);
+const request = new UploadRequest(storage, virus_scanner, image_scaler);
