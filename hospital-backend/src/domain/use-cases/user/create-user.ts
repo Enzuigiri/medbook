69074513153b97d10d38e5b501bf060468e273e6
Enzuigiri <zulfadliyunir.08@gmail.com>
2024-01-09 16:32:27 +0700

    feat:  add validator in created user

--- hospital-backend/src/domain/use-cases/user/create-user.ts
+++ hospital-backend/src/domain/use-cases/user/create-user.ts
@@ -1,3 +1,4 @@
+import { MedicalRecord } from "../../entities/medical-record";
 import { User } from "../../entities/user";
 import { UserRepository } from "../../interfaces/repositories/user-repository";
 import { CreateUserUseCase } from "../../interfaces/use-cases/user/create-user";
@@ -9,6 +10,10 @@ export class CreateUser implements CreateUserUseCase {
   }
 
   async execute(user: User): Promise<Boolean> {
+    user.created_at = new Date()
+    user.last_login = new Date()
+    user.hospital_request = []
+    user.medical_record = { medications: [] }
     const result = await this.userRepository.createUser(user);
     return result;
   }
--- hospital-backend/src/presentation/routers/user-router.ts
+++ hospital-backend/src/presentation/routers/user-router.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from "express";
-import validator from 'validator';
+import { body, validationResult } from "express-validator";
 import { GetAllUsersUseCase } from "../../domain/interfaces/use-cases/user/get-all-users.js";
 import { CreateUserUseCase } from "../../domain/interfaces/use-cases/user/create-user.js";
 
@@ -9,24 +9,36 @@ export default function UserRouter(
 ) {
   const router = express.Router();
 
-  router.get("/", async (req: Request, res: Response) => {
-    try {
-      const users = await getAllUsersUseCase.execute();
-      res.send(users);
-    } catch (err) {
-      res.status(500).send({ message: "Error fetching data" });
+  router.get( "/", async (req: Request, res: Response) => {
+      try {
+        const users = await getAllUsersUseCase.execute();
+        res.send(users);
+      } catch (err) {
+        res.status(500).send({ message: "Error fetching data" });
+      }
     }
-  });
+  );
 
-  router.post("/", async (req: Request, res: Response) => {
-    try {
-      let body = req.body
-      await createUserUseCase.execute(req.body);
-      res.status(201).send({ message: "Created" });
-    } catch (err) {
-      res.status(500).send({ message: "Error fetching data " });
+  router.post(
+    "/",
+    body("email").isEmail().notEmpty().escape(),
+    body("password").isString().notEmpty().escape(),
+    body("name").isString().notEmpty().escape(),
+    async (req: Request, res: Response) => {
+      try {
+        const result = validationResult(req);
+        
+        if (result.isEmpty()) {
+          await createUserUseCase.execute(req.body);
+          res.status(201).send({ message: "Created" });
+        }
+        
+        return res.status(400).send({ message: "Some data is missing"});
+      } catch (err) {
+        res.status(500).send({ message: "Error fetching data " });
+      }
     }
-  });
+  );
 
   return router;
 }
